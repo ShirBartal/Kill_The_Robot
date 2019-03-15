79535790e30436b3a905d863eb13e0c6a0346f93
Shir Bar Tal <shir.bartal@gmail.com>
2019-03-14 19:11:11 -0700

    Create Arena.h

---
+++ Arena.h
@@ -0,0 +1,43 @@
+#ifndef ARENA_H
+#define ARENA_H
+#include <string>
+#include "globals.h"
+using namespace std;
+class Player;
+class Robot;
+class Game; //just added
+
+class Arena
+{
+public:
+	// Constructor/destructor
+	Arena(int nRows, int nCols);
+	~Arena();
+
+	// Accessors
+	int     rows() const;
+	int     cols() const;
+	Player* player() const;
+	int     robotCount() const;
+	int     nRobotsAt(int r, int c) const;
+	void    display(string msg) const;
+
+	// Mutators
+	bool   addRobot(int r, int c);
+	bool   addPlayer(int r, int c);
+	void   damageRobotAt(int r, int c);
+	bool   moveRobots();
+
+	// History
+	// History& history();
+
+private:
+	int     m_rows;
+	int     m_cols;
+	Player* m_player;
+	Robot*  m_robots[MAXROBOTS];
+	int     m_nRobots;
+
+	// History* m_history;
+};
+#endif
