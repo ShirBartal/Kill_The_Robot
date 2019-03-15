0a2c1c301afbd6c421e3415f641a5032bb561b43
Shir Bar Tal <shir.bartal@gmail.com>
2019-03-14 20:17:30 -0700

    Create Globals.h

---
+++ Globals.h
@@ -0,0 +1,15 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+int decodeDirection(char dir);
+void clearScreen();
+
+const int MAXROWS = 20;             // max number of rows in the arena
+const int MAXCOLS = 40;             // max number of columns in the arena
+const int MAXROBOTS = 130;          // max number of robots allowed
+const int MAXSHOTLEN = 5;           // max number of steps you can shoot
+const int UP = 0;
+const int DOWN = 1;
+const int LEFT = 2;
+const int RIGHT = 3;
+#endif#pragma once
