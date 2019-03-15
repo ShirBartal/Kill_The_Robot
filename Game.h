9dfb696e5feae5fc6e747047ca6aca443899cb19
Shir Bar Tal <shir.bartal@gmail.com>
2019-03-14 20:17:05 -0700

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,18 @@
+#ifndef GAME_H
+#define GAME_H
+class Arena;
+
+class Game
+{
+public:
+	// Constructor/destructor
+	Game(int rows, int cols, int nRobots);
+	~Game();
+
+	// Mutators
+	void play();
+
+private:
+	Arena * m_arena;
+};
+#endif#pragma once
